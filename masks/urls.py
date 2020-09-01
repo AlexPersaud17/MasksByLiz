0b968ec1e58763169ba1f6f275713bd466602004
Alex Persaud <alexpersaud@alexs-mbp-2.lan>
2020-08-31 23:37:31 -0400

    Add logo and favicon

--- masks/urls.py
+++ masks/urls.py
@@ -6,9 +6,5 @@ urlpatterns = [
 
     path('shop/', views.Shop.as_view(), name="shop"),
     path('about/', views.About.as_view(), name="about"),
-    path('cart/', views.CartView, name="cart"),
 
-    path('<int:pk>/', views.MaskDetail.as_view(), name="mask-detail"),
-    path('cart/<slug>', views.add_to_cart, name='add-cart'),
-    path('remove/<slug>', views.remove_from_cart, name='remove-cart'),
 ]
