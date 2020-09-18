2e9603fa0f3aad782cbdd20589e3f2d710574b66
Alex Persaud <alexpersaud@alexs-mbp-2.lan>
2020-09-17 20:03:16 -0400

    css

--- masksbyliz/settings.py
+++ masksbyliz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dx$zzf-g(jfmjai5y_^_cc9ioy+z5eg@pp%h2rsdt87pu)+c!7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
